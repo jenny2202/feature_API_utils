1d19d9336c2923828c42b7ed7bcf606bf09f62f5
tony.trinh <tonytrinh0111@gmail.com>
2022-08-14 21:50:24 +1000

    restructure

---
+++ feature_api/util.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+import requests
+
+FEATURES_API_URL = "https://api.predicthq.com/v1/features"
+ACCESS_TOKEN = ''
+
+def hello_world():
+    return print("Hello world, this is my function working")
